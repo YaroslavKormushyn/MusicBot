9d18c40ddbb30704b97dc446345f37fa883bbbe8
YaroslavKormushyn <kormushyn.slavik@gmail.com>
2017-12-19 01:29:18 +0200

    New version. Add basic messaging.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material/material.module';
+import { ChatComponent } from './components/chat/chat.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dialog-flow-client/dialog-flow.client.ts
@@ -0,0 +1,4 @@
+import { ApiAiClient } from 'api-ai-javascript';
+
+// You'll get this access token from Your Agent Dashboard > Settings > General > API keys > Developer access token
+export const client = new ApiAiClient({ accessToken: '57b2d693b1d04a40a8a31e8c27a2de0e' });
