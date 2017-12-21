2a346eda9b79b96f8de0ec96ea0c2d6feefcffa7
YaroslavKormushyn <kormushyn.slavik@gmail.com>
2017-12-21 19:51:25 +0200

    Updated.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,5 +6,27 @@ export const environment = {
     {
       angularBot: '57b2d693b1d04a40a8a31e8c27a2de0e'
     },
-  prologTimeout: 3000
+  prologTimeout: 3000,
+  reply: {
+    'welcome': [
+      'Hi!',
+      'Hello!',
+      'Nice to meet you!'
+    ],
+    'fallback': [
+      'Sorry, I didn\'t quite catch that. Could you try again?',
+      'I didn\'t understand that. What do you mean?',
+      'Sorry.',
+      'Can you repeat, please?'
+    ],
+    'music.play': [
+      'Okay.'
+    ],
+    'music.play:play-music': [
+      'Nice.'
+    ],
+    'music.search': [
+      'On my way.'
+    ]
+  }
 };
